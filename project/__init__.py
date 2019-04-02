cfdca6fb6d0a56b47e04d51b7679694b9e3cdf08
Shulun Chen <shulun.chen@shulun-chen-mb.local>
2019-04-02 10:34:21 -0700

    Added the ability to connect to POSTGRESQL db on aws rds

--- project/__init__.py
+++ project/__init__.py
@@ -3,12 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
+
+passwd = os.environ.get('DB_passwd')
+
+POSTGRES_ADDRESS = "product-analytics-db-instance.cupv3jj2ht0z.us-west-2.rds.amazonaws.com"
+POSTGRES_PORT = 5432
+POSTGRES_USERNAME = "team9"
+POSTGRES_PASSWORD = passwd
+POSTGRES_DBNAME = "postgres"
+
+postgres_str = 'postgresql+psycopg2://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(
+    username=POSTGRES_USERNAME, 
+    password=POSTGRES_PASSWORD, 
+    ipaddress=POSTGRES_ADDRESS, 
+    port=POSTGRES_PORT, 
+    dbname=POSTGRES_DBNAME
+)
 
 app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/shulun.chen/Documents/school_project/MSDS-603/back_end/project/database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/shulun.chen/Documents/school_project/MSDS-603/back_end/project/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = postgres_str
 app.config['SECRET_KEY'] = '1234567890'
 # app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 # app.config['SERVER_NAME'] = 'local.dev:5000'
--- project/models.py
+++ project/models.py
@@ -1,13 +1,20 @@
-from project import db, bcrypt
+from project import db
+from project import bcrypt as bcrypt_app
+import bcrypt
 from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
 from datetime import datetime
 
 
+schema = 'appdata'
+
 class User(db.Model):
+
+    __tablename__ = schema+'.'+'user'
+
     uuid = db.Column(db.String(100), primary_key=True)
     username = db.Column(db.String(80), primary_key=True)
     email = db.Column(db.String(100), nullable=False)
-    _password = db.Column(db.String(80), nullable=False)
+    p_password = db.Column(db.String(80), nullable=False)
     age = db.Column(db.Integer)
     gender = db.Column(db.String(3))
     city = db.Column(db.String(20))
@@ -29,15 +36,16 @@ class User(db.Model):
     
     @hybrid_property
     def password(self):
-        return self._password
+        return self.p_password
 
     @password.setter
     def password(self, plain_password):
-        self._password = bcrypt.generate_password_hash(plain_password)
+        pwhash = bcrypt.hashpw(plain_password.encode('utf8'), bcrypt.gensalt())
+        self.p_password = pwhash.decode('utf8')
 
     @hybrid_method
     def is_correct_password(self, plain_password):
-        return bcrypt.check_password_hash(self.password, plain_password)
+        return bcrypt_app.check_password_hash(self.password, plain_password)
 
     @property
     def is_authenticated(self):
