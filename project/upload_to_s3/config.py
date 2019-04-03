db65c947f5e4f7e9460746a27ade4a3f5bde7fd2
Shulun Chen <shulun.chen@shulun-chen-mb.local>
2019-04-02 21:14:42 -0700

    Attempt 5 to fix the csrf token missing issue

--- project/upload_to_s3/config.py
+++ project/upload_to_s3/config.py
@@ -5,6 +5,6 @@ S3_KEY = os.environ.get("S3_ACCESS_KEY")
 S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
 S3_LOCATION = "http://{}.s3.amazonaws.com/".format(S3_BUCKET)
 
-SECRET_KEY = os.urandom(32)
+# SECRET_KEY = os.urandom(32)
 DEBUG = True
-PORT = 5000
+# PORT = 5000
