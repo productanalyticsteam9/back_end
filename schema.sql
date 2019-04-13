bccd8ca85b439d7b793bba4d9904838d08cd227d
Bowen Ma <bma10@dons.usfca.edu>
2019-04-12 21:47:57 -0700

    adjust spaces to be consistent

--- schema.sql
+++ schema.sql
@@ -23,28 +23,27 @@ CREATE TABLE IF NOT EXISTS "appdata.user"
 
 CREATE TABLE IF NOT EXISTS "appdata.poll" 
 (
-    poll_id BIGSERIAL NOT NULL
-    , uuid VARCHAR(100) REFERENCES appdata.user(uuid) ON DELETE CASCADE
-    , image_id_a BIGINT NOT NULL DEFAULT NEXTVAL('poll_image_id_seq')
-    , image_id_b BIGINT NOT NULL DEFAULT NEXTVAL('poll_image_id_seq')
-    , image_path_a VARCHAR(100) NOT NULL
-    , image_path_b VARCHAR(100) NOT NULL
-    , vote_a_cnt BIGINT
-    , vote_b_cnt BIGINT
-    , post_date TIMESTAMP WITHOUT TIME ZONE
-    , user_tag TEXT
-    , model_tag TEXT
-    , PRIMARY KEY (poll_id)
-    
+  poll_id BIGSERIAL NOT NULL
+  , uuid VARCHAR(100) REFERENCES appdata.user(uuid) ON DELETE CASCADE
+  , image_id_a BIGINT NOT NULL DEFAULT NEXTVAL('poll_image_id_seq')
+  , image_id_b BIGINT NOT NULL DEFAULT NEXTVAL('poll_image_id_seq')
+  , image_path_a VARCHAR(100) NOT NULL
+  , image_path_b VARCHAR(100) NOT NULL
+  , vote_a_cnt BIGINT
+  , vote_b_cnt BIGINT
+  , post_date TIMESTAMP WITHOUT TIME ZONE
+  , user_tag TEXT
+  , model_tag TEXT
+  , PRIMARY KEY (poll_id)
 );
 
 
 
 CREATE TABLE IF NOT EXISTS "appdata.vote" 
 (
-	uuid BIGINT REFERENCES appdata.user(uuid) ON DELETE CASCADE
-	, voter_poll_id BIGINT
-    , poll_date TIMESTAMP WITHOUT TIME ZONE
+  uuid BIGINT REFERENCES appdata.user(uuid) ON DELETE CASCADE
+  , voter_poll_id BIGINT
+  , poll_date TIMESTAMP WITHOUT TIME ZONE
 );
 
 
