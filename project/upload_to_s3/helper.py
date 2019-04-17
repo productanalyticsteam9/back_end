8cdefef38edcc65dfa6ee2ec192754d837153fb9
Shulun Chen <shulun.chen@shulun-chen-mb.local>
2019-04-16 17:50:10 -0700

    Added new changes

--- project/upload_to_s3/helper.py
+++ project/upload_to_s3/helper.py
@@ -1,39 +1,34 @@
-import boto3, botocore
+# import boto3, botocore
 # from .server import app
 from .. import app
+from ..resources import _get_s3_client
+from flask import redirect
 
 ALLOWED_EXTENSIONS = set(['txt', 'md', 'markdown', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(name):
     return "." in name and name.split(".")[1].lower() in ALLOWED_EXTENSIONS
 
-# Connect to the s3 service
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id = app.config['S3_KEY'],
-    aws_secret_access_key = app.config['S3_SECRET'],
-    region_name = 'us-west-2'
-)
+# Connect and upload to s3
 
 def upload_file_to_s3(file, bucket_name, folder='user', acl='public-read'):
     """
     Docs: http://boto3.readthedocs.io/en/latest/guides/s3.html
     """
+    s3 = _get_s3_client()
     try:
         file_path = "{}/{}".format(folder, file.filename)
         s3.upload_fileobj(
             file,
             bucket_name,
-            file_path,
-            ExtraArgs = {
-                # 'ACL': acl,
-                'ContentType': file.content_type
-            }
+            file_path
         )
+        url = s3.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 
+                                                              'Key': file_path},
+                                        ExpiresIn=604800)
     except Exception as e:
         print("Something happened:", e)
         return e
-
     
-
-    return "{}{}".format(app.config['S3_LOCATION'], file_path)
+    # return "{}{}".format(app.config['S3_LOCATION'], file_path)
+    return url
